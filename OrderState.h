dfbfdc6e42cda7e48aa8c3090f373d35e749e22b
Abdelrahman Habish <u24898008@tuks.co.za>
2025-08-30 11:28:01 +0200

    Implemented the the State hierarchy(Not fully)

---
+++ OrderState.h
@@ -0,0 +1,40 @@
+#ifndef ORDERSTATE_H
+#define ORDERSTATE_H
+
+#include <string>
+#include "Order.h"
+
+using namespace std;
+
+class OrderState
+{
+public:
+    virtual void handleOrder(Order *order) = 0;
+};
+
+class Placed : public OrderState
+{
+public:
+    virtual void handleOrder(Order *order);
+};
+
+class Preparing : public OrderState {
+public:
+    virtual void handleOrder(Order* order);
+};
+
+class QualityCheck : public OrderState {
+public:
+    virtual void handleOrder(Order* order);
+};
+
+class Packing : public OrderState {
+    virtual void handleOrder(Order* order);
+}
+
+class Ready : public OrderState {
+public:
+    virtual void handleOrder(Order* order);
+};
+
+#endif
---
+++ OrdrerState.cpp
@@ -0,0 +1,22 @@
+#include "OrderState.h"
+
+
+void PLaced::handleOrder(Order *order){
+    
+}
+
+void Preparing::handleOrder(Order *order){
+    
+}
+
+void QualityCheck::handleOrder(Order *order){
+    
+}
+
+void Packing::handleOrder(Order *order){
+    
+}
+
+void Ready::handleOrder(Order *order){
+    
+}
